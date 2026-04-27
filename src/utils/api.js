e90101c86ca6a2923d0b2c09bcc6d6257c12a44b
JAY-TECH <64235361+JohnJay10@users.noreply.github.com>
2026-04-27 10:38:10 +0100

    Update API base URL to new endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 const API = axios.create({
   // baseURL: 'http://localhost:3000/api',  
-    baseURL: 'https://ctks-rpxt.onrender.com/api',
+    // baseURL: 'https://ctks-rpxt.onrender.com/api',
+    baseURL: 'https://ctks.onrender.com/api',
   
   headers: {
     'Content-Type': 'application/json',
